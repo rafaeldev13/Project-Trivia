317d4a8e649b2348ff72b3fcb513cc4625d95613
João Vitor <joaovitorfischborn@icloud.com>
2022-08-05 21:30:40 -0300

    Iniciando requisito 06

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      resultAPI: [],
+    };
+  }
+
+  componentDidMount() {
+    this.callingAPI();
+  }
+
+  callingAPI = async () => {
+    const myItem = localStorage.getItem('token');
+    const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${myItem}`);
+    const data = await response.json();
+
+    if (data.results.length === 0) {
+      const { history } = this.props;
+      localStorage.removeItem('token');
+      history.push('/');
+    } else {
+      this.setState({ resultAPI: data.results });
+    }
+  }
+
   render() {
+    const { resultAPI } = this.state;
     return (
-      <div>Tela do Jogo</div>
+      <div>
+        { resultAPI.length > 0 && (
+          <>
+            <p>{resultAPI[0].category}</p>
+            <p>{resultAPI[0].question}</p>
+            <button />
+            <button />
+          </>
+        )}
+      </div>
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Game;
