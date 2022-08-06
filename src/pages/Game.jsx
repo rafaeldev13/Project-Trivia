2e51a3648935620945d3ee47736cf9ff491e6845
João Vitor <joaovitorfischborn@icloud.com>
2022-08-05 21:45:37 -0300

    nem sei oq eu to fazendo

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,12 +4,6 @@ import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
 
 class Game extends React.Component {
-  createImageSrc = () => {
-    const { email } = this.props;
-    const hash = md5(email).toString();
-    return `https://www.gravatar.com/avatar/${hash}`;
-  }
-
   constructor() {
     super();
     this.state = {
@@ -19,6 +13,12 @@ class Game extends React.Component {
 
   componentDidMount() {
     this.callingAPI();
+  }
+
+  createImageSrc = () => {
+    const { email } = this.props;
+    const hash = md5(email).toString();
+    return `https://www.gravatar.com/avatar/${hash}`;
   }
 
   callingAPI = async () => {
