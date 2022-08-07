0ffea7c44a61714a4f715f80d775410e5a91c58d
Henryck <ryck302@gmail.com>
2022-08-07 02:12:52 -0300

    requisito 08

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -10,11 +10,39 @@ class Game extends React.Component {
     this.state = {
       resultAPI: [],
       questionNumber: 0,
+      timer: 30,
+      shuffledOptions: [],
+      isDisabled: false,
+      intervalId: '',
     };
   }
 
   componentDidMount() {
     this.callingAPI();
+    this.myRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    const { timer, isDisabled } = this.state;
+    // é necessário usar os isDisabled, pois caso contrário,
+    // ele ficará entrando continuamente no if, se o timer for igual a zero
+    if (timer === 0 && isDisabled === false) {
+      this.setAsIncorrect();
+    }
+  }
+
+  setAsIncorrect = () => {
+    // pegando o elemento através do Ref
+    const optElement = this.myRef.current;
+    this.changeOptionsColors(optElement);
+  }
+
+  disableButton = () => {
+    const parent = this.myRef.current.parentElement;
+    const options = [...parent.children];
+    options.forEach((opt) => {
+      opt.disabled = true;
+    });
   }
 
   createImageSrc = () => {
@@ -33,8 +61,20 @@ class Game extends React.Component {
       localStorage.removeItem('token');
       history.push('/');
     } else {
-      this.setState({ resultAPI: data.results });
+      console.log('chamei api');
+      this.setState({ resultAPI: data.results }, this.createRandomOptions);
     }
+  }
+
+  initializeTimer = () => {
+    console.log('inicializei o timer');
+    const ONE_SECOND = 1000;
+    const intervalId = setInterval(() => {
+      const { timer } = this.state;
+      this.setState({ timer: timer - 1 });
+    }, ONE_SECOND);
+    // é necessário inserir o intervalId no state para poder parar ele
+    this.setState({ intervalId });
   }
 
   shuffleArray = (array) => {
@@ -47,59 +87,79 @@ class Game extends React.Component {
     return array;
   }
 
-  createRandomOptions = () => {
-    const { resultAPI, questionNumber } = this.state;
-    const options = [];
+  createIncorrectOption = (answer, i) => {
+    const { isDisabled } = this.state;
+    const incorrectOpt = (
+      <button
+        type="button"
+        data-testid={ `wrong-answer-${i}` }
+        key={ i }
+        onClick={ (e) => this.changeOptionsColors(e.target) }
+        name="incorrect"
+        disabled={ isDisabled }
+      >
+        { answer }
+      </button>
+    );
 
-    resultAPI[questionNumber].incorrect_answers.map((answer, i) => {
-      const option = (
-        <button
-          type="button"
-          data-testid={ `wrong-answer-${i}` }
-          key={ i }
-          onClick={ (e) => this.changeOptionsColors(e) }
-          name="incorrect"
-        >
-          { answer }
-        </button>
-      );
-      options.push(option);
-      return option;
-    });
+    return incorrectOpt;
+  }
 
+  createCorrectOption = (answer) => {
+    const { isDisabled } = this.state;
     const correctOpt = (
       <button
         type="button"
         key={ 4 }
         data-testid="correct-answer"
         name="correct"
-        onClick={ (e) => this.changeOptionsColors(e) }
+        onClick={ (e) => this.changeOptionsColors(e.target) }
+        ref={ this.myRef }
+        disabled={ isDisabled }
       >
-        {resultAPI[questionNumber].correct_answer}
+        {answer}
       </button>
     );
-    options.push(correctOpt);
-
-    const shuffledOptions = this.shuffleArray(options);
-    return shuffledOptions;
+    return correctOpt;
   }
 
-  changeOptionsColors = ({ target }) => {
+  createRandomOptions = () => {
+    const { resultAPI, questionNumber } = this.state;
+    let { shuffledOptions } = this.state;
+    const options = [];
+
+    resultAPI[questionNumber].incorrect_answers.forEach((answer, i) => {
+      options.push(this.createIncorrectOption(answer, i));
+    });
+
+    const answer = resultAPI[questionNumber].correct_answer;
+    options.push(this.createCorrectOption(answer));
+
+    shuffledOptions = this.shuffleArray(options);
+    console.log('embaralhei as resposta e adicionei na tela');
+    this.setState({ shuffledOptions }, this.initializeTimer);
+  }
+
+  changeOptionsColors = (target) => {
+    const { intervalId } = this.state;
+    clearInterval(intervalId);
+
     const parent = target.parentElement;
     const optionsArray = [...parent.children];
-    optionsArray.map((option) => {
+    optionsArray.forEach((option) => {
       if (option.name === 'incorrect') {
         option.classList.add('red-border');
       } else {
         option.classList.add('green-border');
       }
-      return option;
     });
+    console.log('botei as bordas');
+    this.setState({ isDisabled: true }, this.disableButton);
   }
 
   render() {
     const { name, score } = this.props;
-    const { resultAPI, questionNumber } = this.state;
+    const { resultAPI, questionNumber, timer, shuffledOptions } = this.state;
 
     return (
       <div>
@@ -110,6 +170,7 @@ class Game extends React.Component {
         />
         <p data-testid="header-player-name">{ name }</p>
         <p data-testid="header-score">{ score }</p>
+        <p>{ timer }</p>
         { resultAPI.length > 0 && (
           <>
             <p data-testid="question-category">{resultAPI[questionNumber].category}</p>
@@ -117,7 +178,7 @@ class Game extends React.Component {
               {he.decode(resultAPI[questionNumber].question)}
             </p>
             <div data-testid="answer-options">
-              { this.createRandomOptions() }
+              { shuffledOptions }
             </div>
           </>
         )}
