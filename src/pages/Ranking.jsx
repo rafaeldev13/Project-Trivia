083eb3ad41e35f2e477fa0b6b61503dd5886fe31
João Vitor <joaovitorfischborn@icloud.com>
2022-08-08 20:25:58 -0300

    requisito 16

---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Ranking extends React.Component {
+  render() {
+    return (
+      <div data-testid="ranking-title">
+        Ranking
+      </div>
+    );
+  }
+}
+
+export default Ranking;
