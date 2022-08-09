bf79a5b9789fe24f950a3cb7af533efcce4d20dd
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-08-09 13:33:19 -0400

    Requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  HandleClickBackToLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <div data-testid="ranking-title">
-        Ranking
-      </div>
+      <>
+        <div data-testid="ranking-title">
+          Ranking
+        </div>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => this.HandleClickBackToLogin() }
+        >
+          Voltar para Home
+        </button>
+      </>
     );
   }
 }
-
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 export default Ranking;
